dfc90d8e7bde0d03e6bff466c084db0233f96a3b
10dimensions <relay.10d@gmail.com>
2025-12-31 20:11:31 +0530

    Implement Lambert Izzo Solver

--- mpitg/src/conic/ballistic.py
+++ mpitg/src/conic/ballistic.py
@@ -5,7 +5,7 @@ import spiceypy as sp
 
 from mpitg.src.almanac.constants import MU_SUN_KM
 from mpitg.src.almanac.ephemeris import load_spice_kernels
-from mpitg.src.solver.lambert import lambert_universal
+from mpitg.src.solver.izzo import lambert_izzo2015
 
 
 # ===========================================================
@@ -13,8 +13,8 @@ from mpitg.src.solver.lambert import lambert_universal
 # ===========================================================
 def compute_ballistic_arc(START_BODY,
                           DESTINATION,
-                          date_start="2024-04-01",
-                          date_end="2024-12-01",
+                          date_start,
+                          date_end,
                         ):
 
     load_spice_kernels()
@@ -23,19 +23,23 @@ def compute_ballistic_arc(START_BODY,
     t_dep = sp.utc2et(date_start)
     t_arr = sp.utc2et(date_end)
 
-    state_E = sp.spkezr(START_BODY, t_dep, "ECLIPJ2000", "NONE", "SUN")[0]
+    state_E = sp.spkezr(START_BODY, t_dep, "ECLIPJ2000", "NONE", "SSB")[0]
     r1 = state_E[:3]
     v1_earth = state_E[3:6]
+
+    print(state_E)
 
     tof = t_arr - t_dep
 
     state_M = sp.spkezr(DESTINATION, t_arr,
-                                "ECLIPJ2000", "NONE", "SUN")[0]
+                                "ECLIPJ2000", "NONE", "SSB")[0]
     r2 = state_M[:3]
     v2_mars = state_M[3:6]
 
+    print(state_M)
+
     try:
-        v1t, v2t = lambert_universal(r1, r2, tof, mu_sun)
+        v1t, v2t = lambert_izzo2015(mu_sun, r1, r2, tof)
     except Exception as e:
         print(e)
 
@@ -55,9 +59,9 @@ def compute_ballistic_arc(START_BODY,
 # MAIN
 # ===========================================================
 if __name__ == "__main__":
-    date_start = "2024-04-01"
-    date_end = "2024-12-01"
-    START_BODY = "EARTH"
+    date_start = "2024-01-01"
+    date_end = "2024-10-10"
+    START_BODY = "EARTH BARYCENTER"
     DESTINATION = "MARS BARYCENTER"
 
     v1t, v2t = compute_ballistic_arc(START_BODY, DESTINATION, date_start, date_end)
